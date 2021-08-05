f147d846e0aa283483dcbedc7b4f3727bf47e2b3
JD <zelenuj@gmail.com>
2021-08-05 18:21:31 +0300

    CU-rga250 Initiall commit v3.0.0

---
+++ README.md
@@ -0,0 +1,18 @@
+#Онлайн консультант (чат) - Jivosite для Opencart 3.x
+
+Оригінальний модуль - https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=30269
+
+##Відмінності від оригіналу:
+- додана українська мова
+- можливість встановлювати за допомогою Composer
+
+## How to install
+- composer require kotygor/opencart-jivosite
+
+"repositories":
+[
+    {
+        "type": "git",
+        "url": "https://github.com/kotygor/opencart-jivosite"
+    }
+]
---
+++ upload/admin/language/uk-ua/extension/module/jivosite.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']    = 'Jivosite - онлайн консультант';
+$_['heading_title2']    = 'Jivosite - онлайн консультант';
+
+// Text
+$_['text_extension']   = 'Модулі';
+$_['text_success']     = 'Налаштування модуля обновлені!';
+$_['text_edit']        = 'Редагування модуля <a target="_blank" style="color:#00bf54; text-decoration:underline" href="https://www.jivosite.ru?partner_id=2469&lang=ru&pricelist_id=5" >Jivosite - онлайн консультант</a>';
+$_['text_edit2']        = '<a target="_blank" style="color:#00bf54; text-decoration:underline" href="https://goo.gl/qym630" >Jivosite - онлайн консультант</a>';
+
+$_['entry_helpm']     = 'Якщо у вас ще немає облікового запису, то ви будете автоматично зареєстровані';
+$_['entry_helpp']     = 'Введіть пароль від облікового запису чи вкажіть новий, якщо ви ще не зареєстровані';
+
+$_['entry_donate']     = 'Якщо ви використовуєте дане доповнення, будь ласка, відправте подяку <a href="https://store.pe-art.ru/jivosite-donate" target="_blank">по цьому посиланню</a>. Дякую :)';
+$_['entry_cop']     = '©<a href="https://store.pe-art.ru/" target="_blank">ART&PR Studio</a>';
+
+// Entry
+$_['entry_status']     = 'Відображення на сайті';
+$_['entry_email']     = 'Email';
+$_['entry_userPassword']     = 'Пароль';
+$_['entry_userDisplayName']     = 'Оператор';
+
+// Error
+$_['error_permission'] = 'Не достатньо прав для управління цим модулем!';
+
+$_['button_nastr']     = 'Налаштування';
+$_['button_setup']     = 'Увійти або зареєструватись';
+$_['button_newwind']     = 'Особитстий кабінет (у новому вікні)';
+$_['button_newwind2']     = 'Відрити особистий кабінет jivosite в окремому вікні';
